c2ea5c72a84d0474ba3011769d15c82a5ac98b9d
Muhammad Anus <80568552+Muhammad-Anus-Developer@users.noreply.github.com>
2021-10-30 17:18:11 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Structure from './components/structure';
+// import Structure from './components/structure';
+
+function App() {
+  return (
+<div className="App">
+
+<Structure />
+
+</div> 
+
+  );
+}
+
+export default App;
